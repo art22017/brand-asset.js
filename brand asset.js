258b548eb36c792d40e29bd137d0ba2494160924
artem <art22017@yandex.ru>
2021-06-26 22:12:49 +0300

    Update

--- brand asset.js
+++ brand asset.js
@@ -1,12 +1,17 @@
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function include(url) {
+  var script = document.createElement('script');
+  script.src = url;
+  document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 var samsung = ["Samsung", "http://samsung.com"]
 
-const ba = (s, h, w)=>{
+const ba = (s, w)=>{
   if (s == samsung)
   {
     var rndm = getRandomInt(1, 1000);
@@ -14,8 +19,10 @@ const ba = (s, h, w)=>{
     img.className = rndm;
     img.src = "icons/Samsung_Logo.svg";
     img.alt = "";
-    img.style.widht = w;
-    img.style.height = h;
+    if (w == null) {
+      w = "250px"
+    }
+    img.style.width = w;
 
     let a = document.createElement('a');
     a.href = s[1];
@@ -25,4 +32,3 @@ const ba = (s, h, w)=>{
     document.body.append(a);
   }
 }
-ba(samsung, '100px', '100px');
